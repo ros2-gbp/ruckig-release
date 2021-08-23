44f21be3f5791c5267939e87b9e4e1d36ee3f9d2
Lars Berscheid <lars.berscheid@kit.edu>
2021-08-23 10:23:08 +0200

    Rebase from 'rpm/noetic/ruckig'

--- include/ruckig/output_parameter.hpp
+++ include/ruckig/output_parameter.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <array>
+#include <type_traits>
 
 #include <ruckig/trajectory.hpp>
 
@@ -10,13 +11,16 @@ namespace ruckig {
 //! Output type of the OTG
 template<size_t DOFs>
 struct OutputParameter {
-    template<class T> using Vector = std::array<T, DOFs>;
-    static constexpr size_t degrees_of_freedom {DOFs};
+    template<class T> using Vector = typename std::conditional<DOFs >= 1, std::array<T, DOFs>, std::vector<T>>::type;
+    size_t degrees_of_freedom;
 
     Vector<double> new_position, new_velocity, new_acceleration;
 
     //! Was a new trajectory calculation performed in the last cycle?
     bool new_calculation {false};
+
+    //! Was the trajectory calculation interrupted? (not yet used)
+    bool was_calculation_interrupted {false};
 
     //! Computational duration of the last update call
     double calculation_duration; // [µs]
@@ -26,6 +30,16 @@ struct OutputParameter {
 
     //! Current time on trajectory
     double time;
+
+    template <size_t D = DOFs, typename std::enable_if<D >= 1, int>::type = 0>
+    OutputParameter(): degrees_of_freedom(DOFs) { }
+
+    template <size_t D = DOFs, typename std::enable_if<D == 0, int>::type = 0>
+    OutputParameter(size_t dofs): degrees_of_freedom(dofs), trajectory(Trajectory<0>(dofs)) {
+        new_position.resize(dofs);
+        new_velocity.resize(dofs);
+        new_acceleration.resize(dofs);
+    }
 };
 
 } // namespace ruckig
